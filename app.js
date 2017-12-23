31d8bca0c33c5c26d667e39dea01d566b3e589df
zhang <chstz_365@163.com>
2017-12-23 22:44:12 +0800

    changed app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+/* 
+* @Author: anchen
+* @Date:   2017-12-23 21:35:54
+* @Last Modified by:   anchen
+* @Last Modified time: 2017-12-23 22:39:57
+*/
+
+$(document).ready(function(){
+    
+});
+console.log('hello world!');
